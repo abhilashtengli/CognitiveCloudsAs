4dc32894e03d5b4cf0f5e453e5f2a534ea9ba642
Abhilash Tengli <122851437+abhilashtengli@users.noreply.github.com>
2023-10-21 11:55:38 +0530

    Add files via upload

---
+++ Sorting/MergeSort.java
@@ -0,0 +1,44 @@
+package Sorting;
+
+public class MergeSort {
+    public static void mergeSort(int[] arr) {
+        if (arr.length > 1) {
+            int mid = arr.length / 2;
+            int[] left = new int[mid];
+            int[] right = new int[arr.length - mid];
+
+            System.arraycopy(arr, 0, left, 0, mid);
+            System.arraycopy(arr, mid, right, 0, arr.length - mid);
+
+            mergeSort(left);
+            mergeSort(right);
+
+            int i = 0, j = 0, k = 0;
+            while (i < left.length && j < right.length) {
+                if (left[i] < right[j]) {
+                    arr[k++] = left[i++];
+                } else {
+                    arr[k++] = right[j++];
+                }
+            }
+
+            while (i < left.length) {
+                arr[k++] = left[i++];
+            }
+
+            while (j < right.length) {
+                arr[k++] = right[j++];
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = { 64, 34, 25, 12, 22, 11, 90 };
+        mergeSort(arr);
+
+        System.out.println("Sorted array:");
+        for (int value : arr) {
+            System.out.print(value + " ");
+        }
+    }
+}
